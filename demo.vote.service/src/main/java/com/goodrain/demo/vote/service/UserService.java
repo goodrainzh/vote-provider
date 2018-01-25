58c418180c0824ff67021cdafe3d6a8bdbfcab1d
zhengh <zhengh@goodrain.com>
2018-01-25 12:07:46 +0800

    no message

--- demo.vote.service/src/main/java/com/goodrain/demo/vote/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.goodrain.demo.vote.service;
-
-public interface UserService {
-	public Integer registerUser(String name);
-
-	public String getUser(Integer id);
-}
--- demo.vote.service/src/main/java/com/goodrain/demo/vote/service/VoteService.java
+++
@@ -1,7 +0,0 @@
-package com.goodrain.demo.vote.service;
-
-public interface VoteService {
-	public String agree(String name, String topic);
-
-	public String disagree(String name, String topic);
-}
